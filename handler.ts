407e0d1493b0a9835c22298cb812d31d6d89433c
Elizabeth Gagne <EGagne-c@scrippsnetworks.com>
2017-03-09 13:18:39 -0500

    Removed global timestamp variable

--- handler.ts
+++ handler.ts
@@ -3,8 +3,6 @@ import { Kinesis } from 'aws-sdk';
 import { Context, Callback } from 'aws-lambda';
 import * as uuid from 'uuid';
 
-var timestamp = 0;
-
 export function hello(event: IEventPayload, context, callback: ICallback) {
   callback(undefined, {
     statusCode: 200,
@@ -28,8 +26,8 @@ export function dataReceiver(event: any, context: Context, callback: Callback) {
     StreamName: process.env.KINESIS_STREAM
   };
 
-  timestamp = Date.now();
-  console.log(`Sending to Kinesis current_time (ms): ${timestamp}`);
+  console.log(`Sending to Kinesis current_time (ms): ${Date.now()}`);
+
   kinesis.putRecord(params).promise().then((data) => {
     callback(undefined, { message: 'Data successfully written to Kinesis stream "data-receiver"' });
   }).catch((err) => {
@@ -40,10 +38,9 @@ export function dataReceiver(event: any, context: Context, callback: Callback) {
 
 
 export function logger(event: any, context: Context, callback: Callback) {
-  var round_trip_time = Date.now() - timestamp;
 
   // print out the event information on the console (so that we can see it in the CloudWatch logs)
-  console.log(`Kinesis round-trip-time (ms): ${round_trip_time}, timestamp (ms): ${timestamp}`);
+  console.log(`Kinesis current_time (ms): ${Date.now()}`);
   console.log(`The following data was written to the Kinesis stream "data-receiver":\n${JSON.stringify(event.Records[0].kinesis, null, 2)}`);
 
   callback(undefined, { event });
